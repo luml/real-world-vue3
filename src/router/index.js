18ac2b15f2e6c9afa8ec50fc51081dfcd63ccf4d
Elio Lu <1520622848@qq.com>
2021-08-21 15:07:13 +0800

    fix: notfound

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventDetails from '../views/EventDetails.vue'
 import About from '../views/About.vue'
+import NotFoundComponent from '../views/NotFoundComponent.vue'
 
 const routes = [
   {
@@ -25,6 +26,10 @@ const routes = [
     // component: () =>
     //   import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/:pathMatch(.*)',
+    component: NotFoundComponent
+  }
 ]
 
 const router = createRouter({
