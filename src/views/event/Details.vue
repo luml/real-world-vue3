127e3419d32cd195937c002453d86a1d1a30b6d0
luml <1520622848@qq.com>
2021-08-26 15:44:17 +0800

    feat: :octocat: nested router

---
+++ src/views/event/Details.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="event">
+      <h1 class="animate__animated animate__pulse">{{ event.title }}</h1>
+      <p>@{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+      <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['event'],
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/event/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="event">
+      <p>Register for {{ event.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['event'],
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
