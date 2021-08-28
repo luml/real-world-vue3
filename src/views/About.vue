4149b74695d2bcf54099730746f6b8fcfde23a09
Elio Lu <1520622848@qq.com>
2021-08-28 14:57:20 +0800

    .

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1 class="animate__animated animate__backInDown">A site for goods(vue mastery)</h1>
+    <h1 class="animate__animated animate__backInDown">
+      A site for goods(vue mastery)
+    </h1>
   </div>
 </template>
